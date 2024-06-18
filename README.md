75455d901a1f105f9a7bc9dd046379244bd3ad41
Shema Elisa <163114861+Ikuzweshema@users.noreply.github.com>
2024-06-18 20:15:39 +0200

    init

--- README.md
+++ README.md
@@ -6,18 +6,3 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
-## Getting Started
-
-First, run the development server:
-
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
-```
-
-Open [http://localhost:3000](http://localhost:5173) with your browser to see the result.
--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,18 @@ import Navbar from "./Components/Navbar";
 import Home from "./Components/Home";
 import Details from "./Components/Details";
 import Favorites from "./Components/Favorites";
+
 function App() {
   return (
     <>
-      <div className="min-h-screen p-6 bg-white text-gray-600 text-lg">
+      <div>
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/recipe-item/:id" element={<Details />} />
           <Route path="/favorites" element={<Favorites />} />
         </Routes>
+        
       </div>
     </>
   );
